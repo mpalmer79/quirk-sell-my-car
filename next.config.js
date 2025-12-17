b00593bfc803ff46bb2660b8cd5bf754e0fe5681
mpalmer79 <mpalm2025@gmail.com>
2025-12-17 15:06:44 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,25 @@
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
+  },
+};
+
+module.exports = nextConfig;
