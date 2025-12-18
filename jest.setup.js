6386242afa2bea2fd8bb4ee1f9e9c54c6074b81f
mpalmer79 <mpalm2025@gmail.com>
2025-12-18 10:07:26 -0500

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -17,8 +17,9 @@ jest.mock('next/navigation', () => ({
 jest.mock('next/image', () => ({
   __esModule: true,
   default: (props) => {
+    const { fill, priority, ...rest } = props;
     // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
-    return <img {...props} />;
+    return <img {...rest} />;
   },
 }));
 
@@ -31,19 +32,29 @@ Object.defineProperty(window, 'scrollTo', {
   writable: true,
 });
 
-// Mock navigator.share
-Object.defineProperty(navigator, 'share', {
-  value: jest.fn(),
-  writable: true,
-});
-
-// Mock window.print
-Object.defineProperty(window, 'print', {
-  value: jest.fn(),
-  writable: true,
-});
+// Mock scrollIntoView
+Element.prototype.scrollIntoView = jest.fn();
 
 // Reset mocks before each test
 beforeEach(() => {
   jest.clearAllMocks();
+});
+
+// Suppress console errors in tests (optional - remove if you want to see them)
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (
+      typeof args[0] === 'string' &&
+      args[0].includes('Warning: An update to') &&
+      args[0].includes('inside a test was not wrapped in act')
+    ) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
 });
