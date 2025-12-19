60ce298b4082eba89b39fe0e3429c139919bf054
mpalmer79 <mpalm2025@gmail.com>
2025-12-19 17:05:14 -0500

    Create index.ts

---
+++ src/components/admin/index.ts
@@ -0,0 +1,5 @@
+export { AnalyticsCards } from './AnalyticsCards';
+export { OffersFilters } from './OffersFilters';
+export { OffersTable } from './OffersTable';
+export { OfferDetailModal } from './OfferDetailModal';
+export { StatusBadge } from './StatusBadge';
